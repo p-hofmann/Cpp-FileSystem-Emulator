d718e9b15137ee3a0fca4445506d69eb1701f6a1
hofmann <p.hofmann.email@gmail.com>
2019-01-10 14:03:29 +0100

    Initial commit

---
+++ include/filesystem.h
@@ -0,0 +1,238 @@
+/**
+ * Created by Peter Hofmann on 09.01.19.
+ */
+
+#ifndef VALIDATOR_FILESYSTEM_H
+#define VALIDATOR_FILESYSTEM_H
+
+#include <sys/stat.h>
+#include <sstream>
+#include <string>
+#include <vector>
+
+/**
+ * filesystem.h
+ *
+ * Emulates '<experimental/filesystem>' without the need for g++17 and additional libraries.
+ * Not all methods are implemented as they are made as they are needed.
+ * Some additional methods are available, such as get_full_path().
+ */
+namespace filesystem
+{
+    class path : public std::string
+    {
+    private:
+        const char _directoryDelimiter;
+        std::string _root_name;
+        std::string _root_directory;
+
+        /**
+         * Remove all delimiter at the end of the string
+         */
+        void _rstrip()
+        {
+            while (this->size() > 1 && this->back() == _directoryDelimiter)
+                this->pop_back();
+        }
+
+        void _init_root();
+
+    public:
+        path();
+
+        path(const path &location);
+
+        path(const std::string &location);
+
+        bool is_absolute();
+
+        bool is_executabe();
+
+        /**
+         * Overwrite current path with another
+         * @param location - Directory or file path
+         * @return - New Path
+         */
+        path operator=(const path &location)
+        {
+            return std::string::operator=(std::string(location.c_str()));
+        }
+
+        /**
+         * Concatenation of paths, separated with a directory delimiter.
+         * @param location - Directory or file path
+         * @return - New Path
+         */
+        path operator/(const path &location)
+        {
+            if (this->empty())
+                return location;
+            if (location.empty())
+                return *this;
+            if (this->back() == _directoryDelimiter)
+                return this->string() + std::string(location.c_str());
+            return this->string() + std::string(1, _directoryDelimiter) + std::string(location.c_str());
+        }
+
+        /**
+         * Get method for _root_name
+         * Unix: ''
+         * Windows: 'C:'
+         * @return - Root of path
+         */
+        std::string root_name() { return _root_name; }
+
+        /**
+         * Get method for _root_directory
+         * Unix: '/'
+         * Windows: '\'
+         * @return - Root directory
+         */
+        std::string root_directory() { return _root_directory; }
+
+        /**
+         * Get method for root path
+         * Root name + root directory
+         * Unix: '/'
+         * Windows: 'c:\'
+         * @return - Root Path
+         */
+        path root_path() { return root_name() + root_directory(); }
+
+        /**
+         * Check if path has a root name.
+         * Linux: Always false.
+         * Windows: True, if absolute path
+         */
+        bool has_root_name() { return !root_name().empty(); }
+
+        /**
+         * Check if path has a root directory.
+         * Linux: True, if absolute path
+         * Windows: True, if absolute path
+         */
+        bool has_root_directory() { return !root_directory().empty(); }
+
+        /**
+         * Check if path has a root path.
+         * Linux: True, if absolute path
+         * Windows: True, if absolute path
+         */
+        bool has_root_path() { return has_root_name() || has_root_directory(); }
+
+        /**
+         * Check if path starts with a specific string
+         * @param value - Value tested against path
+         * @return - True if path is sub string of value at the beginning
+         */
+        bool startsWith(const std::string &value)
+        {
+            return rfind(value, 0) == 0;
+        }
+
+        /**
+         * Check if path ends with a specific string
+         * @param value - Value tested against path
+         * @return - True if path is sub string of value at the end
+         */
+        bool endsWith(const std::string &value)
+        {
+            if (length() < value.length())
+                return false;
+            return (0 == compare(length() - value.length(), value.length(), value));
+        }
+
+        /**
+         * Same as 'basename'
+         * Return the folder or file name without the rest of the path
+         * @return - Folder or file name
+         */
+        path filename()
+        {
+            size_type delimiterPosition = this->rfind(_directoryDelimiter);
+            if (delimiterPosition == std::string::npos)
+                return *this;
+            return this->substr(delimiterPosition + 1);
+        }
+
+        /**
+         * Same as 'dirname'
+         * Return the parent folder path without the last path element
+         * @return - Parent folder path
+         */
+        path parent_path()
+        {
+            size_type delimiterPosition = this->rfind(_directoryDelimiter);
+
+            /// No parent folder
+            if (delimiterPosition == std::string::npos)
+                return path("");
+            path result = this->substr(0, delimiterPosition);
+
+            /// Is at root
+            if (result.empty())
+                return root_path();
+            result._rstrip();
+
+            /// Parent folder
+            return result;
+        }
+
+        path get_full_path();
+
+        /**
+         * Return copy of path as string obj.
+         * @return
+         */
+        std::string string() { return std::string(this->c_str()); }
+    };
+
+    /**
+     * Check if a file or folder exists at the path location
+     * @param location - File or folder path
+     * @return - True if file or folder exists at the path location
+     */
+    inline bool exists(const path &location)
+    {
+        struct stat fileAttributes;
+        return stat(location.c_str(), &fileAttributes) == 0;
+    }
+
+    /**
+     * Check if there is a file at a location
+     * @param location - File or folder path
+     * @return  - True if file exists at the path location
+     */
+    inline bool is_regular_file(const path &location)
+    {
+        struct stat fileAttributes;
+        if (stat(location.c_str(), &fileAttributes) != 0)
+        {
+            return false;
+        }
+        return (fileAttributes.st_mode & S_IFREG) != 0;
+    }
+
+    /**
+     * Check if there is a folder at a location
+     * @param location - File or folder path
+     * @return  - True if folder exists at the path location
+     */
+    inline bool is_directory(const path &name)
+    {
+        struct stat fileAttributes;
+        if (stat(name.c_str(), &fileAttributes) != 0)
+            return false;
+        return (fileAttributes.st_mode & S_IFDIR) != 0;
+    }
+
+    std::vector<std::string> _getSystemPaths();
+
+    path current_path();
+
+    path get_environment_variable(std::string const &input);
+
+//    temp_directory_path
+}
+
+#endif //VALIDATOR_FILESYSTEM_H
---
+++ src/filesystem_WIN32.cpp
@@ -0,0 +1,203 @@
+/**
+ * Created by Peter Hofmann on 09.01.19.
+ */
+
+#include <direct.h>
+#include <vector>
+#include <stdexcept>
+#include "filesystem.h"
+
+namespace filesystem
+{
+    /**
+     * Windows specific value initiation
+     */
+    void path::_init_root()
+    {
+        if (location.size() > 2)
+            /* X:\ hopefully indicated root path enough for a windows system */
+            if (location[1] == ':' && location[2] == _directoryDelimiter)
+            {
+                _root_name = substr(0, 2);
+                _root_directory = std::string(1, _directoryDelimiter);
+            }
+    }
+
+    /**
+     * Constructor with no pathing argument
+     */
+    path::path()
+            : std::string(), _directoryDelimiter('\\'), _root_name(""), _root_directory("") { _rstrip(); }
+
+    /**
+     * Constructor with file or folder location parameter
+     * @param location - file or folder location
+     */
+    path::path(const path &location)
+            : std::string(location), _directoryDelimiter('\\'), _root_name(""), _root_directory("")
+    {
+        _init_root();
+        _rstrip();
+    }
+
+    /**
+     * Constructor with file or folder location parameter
+     * @param location - file or folder location
+     */
+    path::path(const std::string &location)
+            : std::string(location), _directoryDelimiter('\\'), _root_name(""), _root_directory("")
+    {
+        if (location.front() == _directoryDelimiter)
+            _root_directory = std::string(1, _directoryDelimiter);
+        _init_root();
+        _rstrip();
+    }
+
+    /**
+     * Return if current path includes the root path
+     * @return
+     */
+    bool path::is_absolute()
+    {
+        return has_root_directory() && has_root_name();
+    }
+
+    /**
+     * Get the full path of any file or directory
+     *
+     * This method tries to forcefully a absolute path for the current path.
+     * Relative paths are looked up locally, then in the system paths.
+     * TODO:
+     *  expandvars - Replace all environment variables
+     *  normpath   - Collapsing redundant separators
+     *  realpath   - Eliminating any symbolic links encountered in the path
+     *
+     * @param path - Directory path or file path
+     * @return - full path to location
+     */
+    path path::get_full_path()
+    {
+        filesystem::path systemPath, possiblePath;
+        std::vector<std::string> systemPathVector;
+
+        if (this->empty())
+            return filesystem::path("");
+
+        filesystem::path location(*this);
+        /// expanduser
+        // %UserProfile% / %HomeDrive%%HomePath% %LOCALAPPDATA% / %APPDATA%
+        std::string homeVariable = "%HomeDrive%%HomePath%";
+        if (startsWith(homeVariable))
+        {
+            location = get_environment_variable("HomeDrive") + get_environment_variable("HomePath") +
+                       substr(homeVariable.size());
+        }
+
+        filesystem::path parentPath = location.parent_path();
+        filesystem::path fileName = location.filename();
+//    std::cout << "'"<< parentPath << "' + '" << fileName << "'" << std::endl;
+
+        /// Path is fullPath, beware: read_symlink, is_symlink
+        if (parentPath.is_absolute())
+            return parentPath / fileName;
+
+        possiblePath = filesystem::current_path() / location;
+        /// Existing CWD + relative path
+        if (filesystem::exists(possiblePath))
+            return possiblePath;
+
+
+        /// check if it is an executable, found in a system path
+        if (parentPath.empty() && !fileName.is_absolute() && !filesystem::exists(fileName))
+        {
+            systemPathVector = _getSystemPaths();
+            for (auto systemPathIt = systemPathVector.begin(); systemPathIt < systemPathVector.end(); systemPathIt++)
+            {
+                // strip('"') ???
+                systemPath = filesystem::path(*systemPathIt);
+                if (filesystem::is_regular_file(systemPath / fileName))
+                {
+                    return systemPath / fileName;
+                }
+            }
+        }
+        /// CWD + relative path, does not exist, no file or folder at path
+        return possiblePath;
+    }
+
+    /**
+     * Tests if a file or directory has permissions to be executed
+     * @return True if executable
+     */
+    bool path::is_executabe()
+    {
+        auto fullPath = get_full_path();
+        return access(fullPath.c_str(), X_OK) == 0;
+    }
+
+    /**
+     * This splits a string
+     * Splitting of the string at a specific delimiter results into multiple strings that are returned as a vector.
+     * @param input - String to be split
+     * @param delimiter - Positions at which input is split, excluding the delimiter.
+     * @return - Vector of Strings
+     */
+    std::vector<std::string> _explode(std::string const &input, char delimiter)
+    {
+        std::vector<std::string> result;
+        std::istringstream iss(input);
+
+        for (std::string token; std::getline(iss, token, delimiter);)
+        {
+            result.push_back(std::move(token));
+        }
+
+        return result;
+    }
+
+    /// OS specific delimiter used in PATH environment variable
+    const char pathDelimiter = ';';
+
+    /**
+     * Returning list of system paths
+     * Reading the PATH environment variable, a list of those paths is returned.
+     * @return - Vector of Strings, each representing a system path.
+     */
+    std::vector<std::string> _getSystemPaths()
+    {
+        //const std::string PATH = convert_to_utf8( _wgetenv(L"PATH") ); // Handle Unicode, just remove if you don't want/need this. convert_to_utf8 uses WideCharToMultiByte in the Win32 API
+        char *tmp;
+        std::string systemPath;
+        tmp = std::getenv(std::string("PATH").c_str());
+        if (tmp == NULL)
+            systemPath = std::string("");
+        else
+            systemPath = std::string(tmp);
+
+        return _explode(systemPath, pathDelimiter);
+    }
+
+    /**
+     * Get current working directory.
+     * @return - Current Working Directory
+     */
+    path current_path()
+    {
+        char buff[FILENAME_MAX];
+        getcwd(buff, FILENAME_MAX);
+        return path(std::string(buff));
+    }
+
+    /**
+     * Get the environment variable
+     * Resolve the environment variable and return it as String
+     * @return - Home directory path
+     */
+    path get_environment_variable(std::string const &input)
+    {
+        const char *variable = getenv(input.c_str());
+        if (variable == NULL)
+            return path("");
+        return path(variable);
+    }
+}
